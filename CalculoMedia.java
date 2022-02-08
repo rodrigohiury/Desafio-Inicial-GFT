7473f2f02d3a6b00b1e2df1686fd4dc75cd07053
Rodrigo <rodrigohiury@alu.ufc.br>
2022-02-08 18:59:49 -0300

    commit 2

--- CalculoMedia.java
+++ CalculoMedia.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class CalculoMedia{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("#.#####");
+        DecimalFormat df = new DecimalFormat("#.#####");    //formata a saída para 5 casas decimais
         System.out.println("Cálculo de Média! Informe a primeira nota: ");
         double a = scanner.nextDouble();
         System.out.println("Informe a segunda nota: ");
         double b = scanner.nextDouble();
+        // O Peso da nota a é de 3,5. E o da nota b é 7,5. Total do pesos é 11
         double media = ((a*3.5)+(b*7.5))/11;
         System.out.println("Média= " + df.format(media));
         System.out.println();
