f3f3c4555112d2adc4e41d034d0b291bb9f661e5
Rodrigo <rodrigohiury@alu.ufc.br>
2022-02-08 21:02:04 -0300

    Comentário duplicado excluído

--- CalculoMedia.java
+++ CalculoMedia.java
@@ -19,7 +19,6 @@ public class CalculoMedia{
         double a = scanner.nextDouble();
         System.out.println("Informe a segunda nota: ");
         double b = scanner.nextDouble();
-        // O Peso da nota a é de 3,5. E o da nota b é 7,5. Total do pesos é 11
         double media = ((a*3.5)+(b*7.5))/11;
         System.out.println("Média= " + df.format(media));
         System.out.println();
